007c2879b1a712e28e853e7a5094c2322f9b4771
Walter Huf <hufman@gmail.com>
2015-04-12 18:22:19 -0500

    Factors out the deeplink loading a bit

--- app/controller.js
+++ app/controller.js
@@ -22,21 +22,33 @@ define(['mithril', './models/schema', 'URI'], function (m, modelSchema, URI) {
 	var data = new modelSchema.Schema();
 	data.supplementalSchemas = m.prop(schemas);	// automatically updating view
 
-	// load up navigation
-	var parseURI = URI(window.location.href);
-	var qsdata = parseURI.query(true);
-	if (qsdata.schemata) {
-		var splitted = qsdata.schemata.split(',');
-		for (var s = 0; s < splitted.length; s++) {
-			var suri = URI(URI.decode(splitted[s]));
-			var schema = addSchema();
-			schema.name(suri.toString());
-			schema.blurName();
+	var loadDeeplink = function() {
+		var ret = {'schemata': [], 'data': null};
+		var parseURI = URI(window.location.href);
+		var qsdata = parseURI.query(true);
+		if (qsdata.schemata) {
+			var splitted = qsdata.schemata.split(',');
+			for (var s = 0; s < splitted.length; s++) {
+				var suri = URI(URI.decode(splitted[s]));
+				ret.schemata.push(suri.toString());
+			}
 		}
+		if (qsdata.data) {
+			var duri = URI(URI.decode(qsdata.data));
+			ret.data=duri.toString();
+		}
+		return ret;
 	}
-	if (qsdata.data) {
-		var duri = URI(URI.decode(qsdata.data));
-		data.name(duri.toString());
+
+	// load up navigation
+	var linkedState = loadDeeplink();
+	for (var s = 0; s < linkedState.schemata.length; s++) {
+		var schema = addSchema();
+		schema.name(linkedState.schemata[s]);
+		schema.blurName();
+	}
+	if (linkedState.data) {
+		data.name(linkedState.data);
 	}
 
 	if (schemas.length < 1) {
