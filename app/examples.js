b5a71acd509c655458264af7f2723438e58ede7b
Walter Huf <hufman@gmail.com>
2015-04-23 01:42:53 -0500

    Adds another example, rfc3339

--- app/examples.js
+++ app/examples.js
@@ -3,6 +3,14 @@ define(['URI'], function(URI) {
 		return URI(rel).absoluteTo(window.location.href).toString();
 	};
 	return [{
+		'title': 'RFC 3339 Timestamp',
+		'schemata': [{
+			'name': abs('examples/rfc3339-schema.json')
+		}],
+		'data': {
+			'name': abs('examples/rfc3339-data.json')
+		}
+		},{
 		'title': 'JSON-RPC',
 		'schemata': [{
 			'name': abs('examples/jsonrpc-request-schema.json')
